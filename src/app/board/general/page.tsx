bd6eb1720d63404d2485c792b30a63b4b0268670
sneed-ay <info@sneed.jp>
2026-04-03 17:19:11 +0900

    feat: add general discussion board category page

---
+++ src/app/board/general/page.tsx
@@ -0,0 +1,5 @@
+import BoardCategoryPage from "@/components/BoardCategoryPage";
+
+export default function GeneralBoardPage() {
+  return <BoardCategoryPage category="general" categoryLabel="雑談" />;
+}
