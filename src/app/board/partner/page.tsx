3330ba7666c1a7e7d41393d73701ecade1d2ace0
sneed-ay <info@sneed.jp>
2026-04-03 17:18:11 +0900

    feat: add partner board category page

---
+++ src/app/board/partner/page.tsx
@@ -0,0 +1,5 @@
+import BoardCategoryPage from "@/components/BoardCategoryPage";
+
+export default function PartnerBoardPage() {
+  return <BoardCategoryPage category="partner" categoryLabel="お相手募集" />;
+}
